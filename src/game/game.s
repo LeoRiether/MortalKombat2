b11650596d67942ac0c3b8ae79e566c465f07905
Leonardo Riether <leonardoalves8217@gmail.com>
2020-12-09 18:52:24 -0200

    Fixed subzero sprite bug

--- src/game/game.s
+++ src/game/game.s
@@ -13,8 +13,8 @@
                   aCrouch aCrouch
                   aKick aKick aKick aKick aKick
         .word 0, 0 # prefix sum needs this
-        player0.sizes: .word 37 99, 37 100, 37 101, 38 101, 39 100, 39 99, 39 100, 38 101, 37 101, 37 100 # aIdle
-                     36 104, 31 104, 31 107, 34 103, 35 103, 31 103, 31 104, 34 105, 43 103 # aWalk
+        player0.sizes: .word 38 99, 38 100, 38 101, 38 101, 40 100, 40 99, 40 100, 38 101, 38 101, 38 100 # aIdle
+                     36 104, 32 104, 32 107, 34 103, 36 103, 32 103, 32 104, 34 105, 44 103 # aWalk
                      40 76, 40 59 # aCrouch
                      40 106, 44 104, 76 92, 38 94, 44 104 # aKick
         player0.sizes_end:
